c55683f52674345550e7b2fb1f8b790e6a76bcb0
Deepan <54735327+deepan4cloud@users.noreply.github.com>
2021-07-18 16:25:36 +0800

    updated region

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg1" {
     name = "terraform-rg"
-    location = "ap-southeast-1"
+    location = "southeastasia"
 }
 
 output "resourcegroup" {
