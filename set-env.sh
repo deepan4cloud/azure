011f5b4e4e92f4cb3aa5e9ea766fe20ab1e2b592
Deepan <54735327+deepan4cloud@users.noreply.github.com>
2021-07-18 13:18:25 +0800

    set-env file

---
+++ set-env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Setting Azure Env Variables"
+export ARM_SUBSCRIPTION_ID=1d0448f9-bdd5-46c2-ba28-81f178bd3a72
+export ARM_CLIENT_ID=e8b7d642-91b1-4554-b0b6-bf878b5045a3
+export ARM_CLIENT_SECRET=2H.tTereYBSIn.vbNEViTOkQImY1x_Jx3o
+export ARM_TENANT_ID=1d790f78-cb86-4b48-89fc-3d03182efbe9
