7de478269e8f967a3076863899af22627f84d1fc
Deepan <54735327+deepan4cloud@users.noreply.github.com>
2021-07-18 16:13:59 +0800

    updated 2

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
 provider "azurerm" {
-    
+    features {}
 }
 
 resource "azurerm_resource_group" "rg1" {
