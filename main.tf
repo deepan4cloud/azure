f3d913c0c23d1949c70258e44224f630d028e5b9
Deepan <54735327+deepan4cloud@users.noreply.github.com>
2021-07-18 18:41:18 +0800

    adding azure_creds

--- main.tf
+++ main.tf
@@ -9,6 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "1d0448f9-bdd5-46c2-ba28-81f178bd3a72"
+  client_id       = "e8b7d642-91b1-4554-b0b6-bf878b5045a3"
+  client_secret   = "2H.tTereYBSIn.vbNEViTOkQImY1x_Jx3o"
+  tenant_id       = "1d790f78-cb86-4b48-89fc-3d03182efbe9"
 }
 
 resource "azurerm_resource_group" "rg1" {
@@ -19,4 +23,3 @@ resource "azurerm_resource_group" "rg1" {
 output "resourcegroup" {
     value = "${azurerm_resource_group.rg1.name}"
 }
-
